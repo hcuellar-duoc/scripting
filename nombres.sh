b9a530416fdecae76c32543769e5b12770264f65
hugo cuellar <root@localhost.localdomain>
2019-09-06 23:30:17 -0300

    cuarta instancia

---
+++ nombres.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "nombres y edades"
+echo "----------------"
+for LINEA in ` cat datos.txt `
+do
+nombre=`echo $LINEA | cut -d ":" -f1`
+edad=`echo $LINEA | cut -d ":" -f2`
+echo "$nombre tiene $edad años"
+done
